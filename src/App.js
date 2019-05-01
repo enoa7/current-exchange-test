aad77b363b4c24c7010db708b3293d54ab43d7d6
Gamal Ardipratama <gamal@aleph-labs.com>
2019-05-01 21:40:50 +0700

    init react app and setup docker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <div>Base Currency</div>
+        <div className="d-flex align-items-center justify-content-between">
+          <div>Currency</div>
+          <div>Amount</div>
+        </div>
       </header>
+      <div className="content">
+        {/* <CurrencyList /> TODO: create currency list component */}
+      </div>
+      <div className="footer"></div>
     </div>
   );
 }
